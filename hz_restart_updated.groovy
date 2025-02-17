9d02b2302e178fde286f717a1b7ed6acce780e1e
shubham joshi <shubham.joshi1507@gmail.com>
2025-02-17 09:26:32 +0530

    Create hz_restart_updated.groovy

---
+++ hz_restart_updated.groovy
@@ -0,0 +1,186 @@
+pipeline {
+    agent { label 'cm-linux' }
+    
+    stages {
+        stage('Server-Restart') { 
+            steps {
+                script {
+                    cleanWs()
+                    deleteDir()
+                    
+                    def userName = ""
+                    def password = ""
+                    
+                    // Extracting folder name dynamically
+                    def getFolder = pwd().split("/")
+                    def foldername = getFolder[getFolder.length - 2]
+
+                    // Cloning repositories
+                    git branch: "main", credentialsId: "HSBCNET-G3-DEV-GITHUB-OAUTH", url: "https://alm-github.systems.uk.hsbc/dtc-hazelcast/Hazelcast-Services.git"
+                    
+                    sh("mkdir jqdir")
+                    
+                    dir('jqdir') {
+                        git branch: "master", credentialsId: "HSBCNET-G3-DEV-GITHUB-OAUTH", url: "https://alm-github.systems.uk.hsbc/sprintnet/jq.git"
+                        
+                        sh """
+                            chmod +x ./jq
+                            mv ./jq ../
+                        """
+                    }
+                    
+                    dir("${WORKSPACE}/${foldername}/") {
+                        sh """
+                            { set +x; } 2>/dev/null;
+                            cat Environments.groovy >> env.txt
+                        """
+                        
+                        def envList = readFile 'env.txt'
+                        
+                        properties([
+                            parameters([
+                                [$class: 'ChoiceParameter',
+                                    choiceType: 'PT_SINGLE_SELECT',
+                                    filterLength: 1,
+                                    filterable: false,
+                                    name: 'Environment',
+                                    script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "return ['Could not get The environments']"
+                                        ],
+                                        script: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "${envList}"
+                                        ]
+                                    ]
+                                ],
+                                [$class: 'ChoiceParameter',
+                                    choiceType: 'PT_SINGLE_SELECT',
+                                    filterLength: 1,
+                                    filterable: false,
+                                    name: 'Action',
+                                    script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [],
+                                        script: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "return ['Could not get The environments']"
+                                        ],
+                                        script: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "return ['start', 'stop', 'restart']"
+                                        ]
+                                    ]
+                                ],
+                                [$class: 'ChoiceParameter',
+                                    choiceType: 'PT_SINGLE_SELECT',
+                                    filterLength: 1,
+                                    filterable: false,
+                                    name: 'Mancenter',
+                                    script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "return ['Could not get The environments']"
+                                        ],
+                                        script: [
+                                            classpath: [],
+                                            sandbox: true,
+                                            script: "return ['false', 'true']"
+                                        ]
+                                    ]
+                                ],
+                                [$class: 'StringParameterDefinition',
+                                    defaultValue: '',
+                                    description: 'Please enter Server Name',
+                                    name: 'Host_Name'
+                                ],
+                                [$class: 'StringParameterDefinition',
+                                    defaultValue: '',
+                                    description: 'Please enter CR Number for Production Deployment',
+                                    name: 'cr_number'
+                                ]
+                            ])
+                        ])
+                    }
+                    
+                    // Setting up environment variables
+                    def deployments = load "${WORKSPACE}/deployment.groovy"
+                    def templateId = "84533"
+                    def jqcli = "${WORKSPACE}/jq"
+                    def containers = "container01"
+                    def file = "Cluster.json"
+                    def clusterSafeUrl = "clusterSafeUrl.json"
+
+                    // Fetch cluster details using jq
+                    def Clusters_List = sh(script: """{ set +x; } 2>/dev/null; cat ${file} | ${jqcli} -r . '${params.Environment}' """, returnStdout: true).trim()
+                    def Cluster_Safe_URL = sh(script: """{ set +x; } 2>/dev/null; cat ${clusterSafeUrl} | ${jqcli} -r . '${params.Environment}' """, returnStdout: true).trim()
+
+                    def Hostname = "${params.Host_Name}"
+                    if (Hostname == "") {
+                        error "Error! Host_Name is Empty. Please enter Host_Name value."
+                    }
+
+                    def environment = "${params.Environment}"
+                    def extravars = """{
+                        "hostname": "${params.Host_Name}",
+                        "clusterName": "${Clusters_List}",
+                        "containerName": "${containers}",
+                        "action": "${params.Action}",
+                        "cluster_safe_url": "${Cluster_Safe_URL}",
+                        "mancenter": "${params.Mancenter}"
+                    }"""
+
+                    // Handling production environment
+                    if (environment.toLowerCase().startsWith("prod")) {
+                        timeout(time: 120, unit: 'SECONDS') {
+                            def userInput = input(id: 'Input-username',
+                                parameters: [
+                                    [$class: 'StringParameterDefinition', defaultValue: '', description: 'Enter Username:', name: 'Username'],
+                                    [$class: 'hudson.model.PasswordParameterDefinition', description: 'Enter Password:', name: 'Password']
+                                ]
+                            )
+                            userName = userInput['Username']
+                            password = userInput['Password'].toString()
+                        }
+                        environment = "prod"
+                        templateId = "28669"
+                        def cr_number = "${params.cr_number}"
+                        if (cr_number == "") {
+                            error "Error! CR Number is Empty for Production Environment."
+                        }
+                        extravars = """{
+                            "cr_number": "${params.cr_number}",
+                            "hostname": "${params.Host_Name}",
+                            "clusterName": "${Clusters_List}",
+                            "containerName": "${containers}",
+                            "action": "${params.Action}",
+                            "cluster_safe_url": "${Cluster_Safe_URL}",
+                            "mancenter": "${params.Mancenter}"
+                        }"""
+                    }
+
+                    println("Extra-Vars are: " + extravars)
+
+                    // Triggering Ansible
+                    def ansibleOutput = deployments.triggerAnsibleTower(templateId, environment, extravars, userName, password)
+
+                    // Debugging ansible output
+                    println "Ansible Output: ${ansibleOutput}"
+
+                    // Check for errors in Ansible output
+                    if (ansibleOutput != null && ansibleOutput.contains("skipping: no hosts matched")) {
+                        error "Error! Ansible playbook output indicates no hosts matched."
+                    }
+                }
+            }
+        }
+    }
+}
